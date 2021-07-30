98f5fc29b978ba56e96f86a8c2a1010db1ae698a
imollov <ivomollov@gmail.com>
2021-07-30 17:34:03 +0300

    test: deploy sample campaign on ganache

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const CampaignFactory = artifacts.require("CampaignFactory");
+const CampaignWallet = artifacts.require("CampaignWallet");
 
-module.exports = async function (deployer) {
+module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(CampaignFactory);
+
+  if (network === 'ganache') {
+    await deployer.deploy(CampaignWallet, [accounts[1], accounts[2]], [50, 50], ['FoodCo', 'BuildCo'], ['Food', 'Construction']);
+    const factory = await CampaignFactory.deployed();
+    const now = Math.floor(new Date().getTime() / 1000);
+    await factory.createCampaign(CampaignWallet.address, 1e18.toString(), now, now + 100000, 'Sample title', 'Sample description');
+  }
 };
