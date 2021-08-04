6c8c2cb39b141949f42152b2724cbb23d3879bc9
imollov <ivomollov@gmail.com>
2021-08-04 18:32:58 +0300

    test(factory): token mint and transfer to campaign

--- test/CampaignFactory.test.js
+++ test/CampaignFactory.test.js
@@ -1,32 +1,42 @@
-const { time } = require('@openzeppelin/test-helpers');
-const { assert } = require('chai');
+const { time, ether } = require('@openzeppelin/test-helpers');
+
+const { expect } = require('chai');
 
 const CampaignFactory = artifacts.require('CampaignFactory');
-const SimpleToken = artifacts.require('ERC20Impl');
+const ERC20 = artifacts.require('ERC20Impl');
 
 contract('CampaignFactory', function (accounts) {
   const [beneficiary] = accounts;
 
   const title = 'Test'
   const description = 'Test';
-  const goal = 1000;
+  const goal = ether('1');
 
   beforeEach(async function () {
     this.factory = await CampaignFactory.deployed();
-    this.token = await SimpleToken.new();
     this.openingTime = (await time.latest()).add(time.duration.weeks(1));
     this.closingTime = this.openingTime.add(time.duration.weeks(1));
   });
 
+  it('should create token', async function() {
+    expect(await this.factory.token()).to.not.be.empty;
+  });
+
   it('should create campaign', async function() {
-    await this.factory.createCampaign(beneficiary, this.token.address, goal,
-      this.openingTime, this.closingTime, title, description);
+    await this.factory.createCampaign(beneficiary, goal, this.openingTime, this.closingTime, title, description);
+  });
+
+  it('should transfer tokens to campaign', async function() {
+    await this.factory.createCampaign(beneficiary, goal, this.openingTime, this.closingTime, title, description);
+    const tokenId = await this.factory.token();
+    const token = await ERC20.at(tokenId);
+    const campaigns = await this.factory.deployedCampaigns();
+    const campaignBalance = await token.balanceOf(campaigns[0]);
+    expect(campaignBalance).to.be.bignumber.equal(goal);
   });
 
   it('should list campaign', async function() {
-    await this.factory.createCampaign(beneficiary, this.token.address, goal,
-      this.openingTime, this.closingTime, title, description);
-
+    await this.factory.createCampaign(beneficiary, goal, this.openingTime, this.closingTime, title, description);
     expect(await this.factory.deployedCampaigns()).to.not.be.empty;
   });
 });
